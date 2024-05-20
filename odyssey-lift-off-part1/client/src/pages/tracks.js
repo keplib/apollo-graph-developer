2fd2bf7529467ebdbedc9f7d58d473c63f5847a1
keplib <keplib@gmail.com>
2024-05-20 11:40:35 +0200

    first commit, completed part 1 and 2

---
+++ odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+  console.log(data);
+
+  if (loading) return 'Loading...';
+
+  if (error) retunr`Error: ${error.message}`;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Tracks;
